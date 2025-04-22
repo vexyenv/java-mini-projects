0a562e42e1f651baeaac0aa1320b02d1d0bcdb7a
vexyenv <vexyenv@outlook.com>
2025-04-22 22:08:26 +0530

    Project - 2 Login System, Simple Calculator v1.2

--- login-system/Login.java
+++ login-system/Login.java
@@ -8,26 +8,39 @@ public class Login {
         String user = "admin@root";
         String pass = "admin-!16@9124";
 
+        boolean success = true;
+
         System.out.println("---------------------");
         System.out.println("[Simple Login System]");
         System.out.println("Enter details below ->");
 
-        System.out.print("Enter username: ");
-        String username = sc.next();
-        System.out.print("Enter password: ");
-        String password = sc.next();
+        int tries = 3;
 
-        if (username.equalsIgnoreCase(user) && password.equals(pass)) {
-            System.out.println("Login Successful");
-            System.out.println("Welcome back " + user + "!");
-        } else if (username.equalsIgnoreCase(user) && !password.equals(pass)) {
-            System.out.println("Correct username, but wrong password...");
-        } else {
-            System.out.println("Invalid Credentials");
-            System.out.println("Forgot password, contact an admin");
+        for (int i = 1; i <= tries; i++ ) {
+            System.out.print("Enter username: ");
+            String username = sc.next();
+            System.out.print("Enter password: ");
+            String password = sc.next();
+
+            if (username.equalsIgnoreCase(user) && password.equals(pass)) {
+                System.out.println("Login Successful");
+                System.out.println("Welcome back " + user + "!");
+                success = true;
+                break;
+            } else if (username.equalsIgnoreCase(user) && !password.equals(pass)) {
+                System.out.println("Correct username, but wrong password...");
+            } else {
+                System.out.println("Invalid Credentials");
+                System.out.println("Forgot password, contact an admin");
+            }
+
+            System.out.println("---------------------");
         }
 
-        System.out.println("---------------------");
+        if (!success) {
+            System.out.println("You failed 3 times!");
+            System.out.println("Account locked");
+        }
 
         sc.close();
     }
--- simple-calculator/Calculator.java
+++ simple-calculator/Calculator.java
@@ -7,34 +7,42 @@ public class Calculator {
         System.out.println("------------------");
         System.out.println("Calculator");
 
-        System.out.print("Enter a number: ");
-        double numA = sc.nextDouble();
-        System.out.print("Enter another number: ");
-        double numB = sc.nextDouble();
-        System.out.println("[Add, Subtract, Divide, Multiply, Remainder]");
-        System.out.print("Choose operator [+,-,/,*,%]: ");
-        String operator = sc.next();
+        for (int i = 1;; i++) {
+            System.out.println("Calculation #" + i);
+            System.out.print("Enter a number: ");
+            double numA = sc.nextDouble();
+            System.out.print("Enter another number: ");
+            double numB = sc.nextDouble();
+            System.out.println("[Add, Subtract, Divide, Multiply, Remainder]");
+            System.out.print("Choose operator [+,-,/,*,%]: ");
+            String operator = sc.next();
 
-        if (operator.equals("+") || operator.equalsIgnoreCase("add")) {
-            System.out.println("Sum = " + (numA + numB));
-        } else if (operator.equals("-") || operator.equalsIgnoreCase("subtract")) {
-            System.out.println("Difference = " + (numA - numB));
-        } else if (operator.equals("/") || operator.equalsIgnoreCase("divide")) {
-            if (numB == 0) {
-                System.out.println("Cannot divide by 0.");
+            if (operator.equals("+") || operator.equalsIgnoreCase("add")) {
+                System.out.println("Sum = " + (numA + numB));
+            } else if (operator.equals("-") || operator.equalsIgnoreCase("subtract")) {
+                System.out.println("Difference = " + (numA - numB));
+            } else if (operator.equals("/") || operator.equalsIgnoreCase("divide")) {
+                if (numB == 0) {
+                    System.out.println("Cannot divide by 0.");
+                } else {
+                    System.out.println("Quotient = " + (numA / numB));
+                }
+            } else if (operator.equals("*") || operator.equalsIgnoreCase("multiply")) {
+                System.out.println("Product = " + (numA * numB));
+            } else if (operator.equals("%") || operator.equalsIgnoreCase("remainder")) {
+                System.out.println("Remainder = " + (numA % numB));
             } else {
-                System.out.println("Quotient = " + (numA / numB));
+                System.out.println("Invalid operator!");
             }
-        } else if (operator.equals("*") || operator.equalsIgnoreCase("multiply")) {
-            System.out.println("Product = " + (numA * numB));
-        } else if (operator.equals("%") || operator.equalsIgnoreCase("remainder")) {
-            System.out.println("Remainder = " + (numA % numB));
-        } else {
-            System.out.println("Invalid operator!");
+
+            System.out.println("------------------");
+
+            System.out.print("Do you want to continue? [yes (y)/no (n)]: ");
+            String option = sc.next();
+            if (option.equalsIgnoreCase("no") || option.equalsIgnoreCase("n")) {
+                System.out.println("Calculator has been closed.");
+                break;
+            }
         }
-
-        System.out.println("------------------");
-
-        sc.close();
     }
 }
